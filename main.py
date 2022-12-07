877db1d3ac6036c4099819be18d81dce52c383bb
Alexandre Chanson <a.chanson.tours@gmail.com>
2022-12-07 14:15:28 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+from subprocess import PIPE, Popen
+
+
+pg_path = "/usr/bin/"
+queries = './workload/queries.txt'
+env_vars = dict(os.environ)
+env_vars['PGPASSFILE'] = './.pgpass'
+
+with Popen([pg_path + "psql", "-h", "localhost", "-U", "marcel", "-d", 'teaching', '-f', queries], env=env_vars, stdout=PIPE, stderr=PIPE) as process:
+    output = process.communicate()[0].decode("utf-8")
+    cost = 0
+    j = 1
+    fetch = False
+    for line in output.splitlines():
+        if line.startswith('-----'):
+            fetch = True
+            continue
+        if fetch:
+            fetch = False
+            t = line.split('..')
+            print('query ', j, ' : ', t[1].split()[0])
+            j = j + 1
+            cost = cost + float(t[1].split()[0])
+
+print('overal cost: ', cost)
+
+# computing size
+with Popen([pg_path + "psql", "-h", "localhost", "-U", "marcel", "-d", 'teaching', '-c', "SELECT pg_size_pretty( pg_database_size('teaching') );"], env=env_vars, stdout=PIPE, stderr=PIPE) as process:
+    output = process.communicate()[0].decode("utf-8")
+    print("Size:", output.splitlines()[2])
+
+# cleaning up
+# os.system("/Applications/Postgres.app/Contents/Versions/11/bin/psql -d 'health insurance' -f /Users/marcel/Documents/ENSEIGNEMENTS/BD/DATASETS/health_insurance/health_insurance.sql")
